9e9868fb7e2b0125fe6ab1118f0e70bcac831fc6
rustyrory <marada49@live.fr>
2026-04-21 16:47:42 +0200

    fix(ux): fixes #93 - header

--- collegeLaBoussoleApp/frontend/src/app/(public)/page.tsx
+++ collegeLaBoussoleApp/frontend/src/app/(public)/page.tsx
@@ -1,19 +1,5 @@
-"use client";
-
 import HomePage from "@/components/home-page";
-import { IconHome, IconLayoutDashboard } from "@tabler/icons-react";
-import { Navbar } from "@/components/site-header";
 
 export default function App() {
-  const menuItems = [
-    { title: "Accueil", url: "/", icon: IconHome },
-    { title: "Dashboard", url: "/dashboard", icon: IconLayoutDashboard },
-  ];
-
-  return (
-    <>
-      <Navbar items={menuItems} />
-      <HomePage />
-    </>
-  );
+  return <HomePage />;
 }
